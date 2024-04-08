effbb1daeb370b57473d1ea4bed47c7ca8cd6598
ssmdbayzid <ssmdbayzid@gmail.com>
2024-04-08 23:24:01 +0600

    update

--- utility/connectData.js
+++ utility/connectData.js
@@ -18,7 +18,4 @@ MONGO_URL = mongodb+srv://dream-house:yUApj5On7Zq9m3Yy@cluster0.yvfoqeg.mongodb.
 JWT_SECRET_KEY = c8a8437677fcfab679fsadas92c8470ffc34b932f5sdfaaa3296c09f65dsf2d2becfe1db8b2dsf
 
 
-  SUCCESS_URL = http://localhost:5173/login/success
- FAILED_URL = http://localhost:5173/login/failed
-
 */
